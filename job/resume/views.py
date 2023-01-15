346027d88cc4d5a942a06a92dcd45c42790fe9f6
Suraj Pathak <surajpathak2057@gmail.com>
2023-01-15 13:13:56 +0545

    Create IsOwner custom permission

--- job/resume/views.py
+++ job/resume/views.py
@@ -1,9 +1,22 @@
 from rest_framework import generics
+from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
 
 from .models import *
 from .serializers import *
+
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:  # Safe=GET
+            return True
+        # Allow write permission to owner only
+        if obj.resume_set.first().user != request.user:
+            raise PermissionDenied(
+                detail="You are not the owner of this resume object."
+            )
+        return True
 
 
 class EducationCreateView(generics.CreateAPIView):
@@ -15,22 +28,10 @@ class EducationCreateView(generics.CreateAPIView):
         education_instance = serializer.save()
         resume, created = Resume.objects.get_or_create(user=self.request.user)
         resume.education.add(education_instance)
-        print(education_instance, resume.education.all())
+        print(education_instance, resume.education.all(), created)
 
 
 class EducationDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
     queryset = Education.objects.all()
     serializer_class = EducationSerializer
-
-    def perform_update(self, serializer):
-        if self.get_object().resume_set.first().user == self.request.user:
-            serializer.save()
-        else:
-            raise PermissionDenied("You are not the owner of this resume")
-
-    def perform_destroy(self, instance):
-        if instance.resume_set.first().user == self.request.user:
-            instance.delete()
-        else:
-            raise PermissionDenied("You are not the owner of this resume")
