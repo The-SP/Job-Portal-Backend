00c225ad790f391333425918c1f94a417a346cca
Suraj Pathak <surajpathak2057@gmail.com>
2023-03-05 18:09:24 +0545

    Add admin site

--- job/user_system/admin.py
+++ job/user_system/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import SeekerProfile, EmployerProfile, UserAccount
 
-# Register your models here.
+admin.site.register(UserAccount)
+admin.site.register(SeekerProfile)
+admin.site.register(EmployerProfile)
--- job/user_system/models.py
+++ job/user_system/models.py
@@ -15,6 +15,17 @@ class UserAccountManager(BaseUserManager):
         user.set_password(password)
         user.save()
         return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if not extra_fields.get('is_staff'):
+            raise ValueError('Superuser must have is_staff=True.')
+        if not extra_fields.get('is_superuser'):
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
