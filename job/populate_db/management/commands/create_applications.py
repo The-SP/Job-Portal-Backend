9fa3bc81c789c2dbc0f4d43d059741b665754b58
Suraj Pathak <surajpathak2057@gmail.com>
2024-01-03 22:33:44 +0545

    Update create_applications management command

--- job/populate_db/management/commands/create_applications.py
+++ job/populate_db/management/commands/create_applications.py
@@ -1,29 +1,52 @@
 from django.core.management.base import BaseCommand
-import random, datetime
-
+import os
+import random
 from user_system.models import SeekerProfile
 from hiring.models import Job, JobApplication
+
 
 class Command(BaseCommand):
     help = "Populate the Job Applications"
 
     def handle(self, *args, **options):
         # Choose job to populate its job_applications
-        JOB_ID = 50
+        JOB_ID = 19733
         job = Job.objects.get(pk=JOB_ID)
+
+        # # Delete all job applications for the selected job
+        # job.applications.all().delete()
+        # self.stdout.write(f"Deleted job applications for job_id {JOB_ID}.")
+        # return
+
         seekers = SeekerProfile.objects.all()
+
+        resume_inputs_path = "media/job_19733_resumes"
+        resume_files = [f for f in os.listdir(resume_inputs_path) if f.endswith(".pdf")]
 
         for seeker in seekers:
             phone_number = seeker.phone_number if seeker.phone_number else ""
 
-            JobApplication.objects.create(
-                user=seeker.user,
-                job=job,
-                name=seeker.name,
-                email=seeker.email,
-                phone_number=phone_number
-            )
+            # Ensure there are resume files available
+            if resume_files:
+                # Assign resumes sequentially to seekers
+                resume_file = resume_files.pop(0)
+                resume_path = os.path.join(resume_inputs_path, resume_file)
 
-            self.stdout.write(
-                f"{seeker.name} sent job application for job_id {JOB_ID}"
-            )
+                resume_path = os.path.join("job_19733_resumes", resume_file)
+
+                JobApplication.objects.create(
+                    user=seeker.user,
+                    job=job,
+                    name=seeker.name,
+                    email=seeker.email,
+                    phone_number=phone_number,
+                    resume=resume_path,
+                )
+
+                self.stdout.write(
+                    f"{seeker.name} sent job application for job_id {JOB_ID} with resume {resume_file}"
+                )
+            else:
+                self.stdout.write(
+                    f"No resume found for {seeker.name}. Skipped job application for job_id {JOB_ID}"
+                )
