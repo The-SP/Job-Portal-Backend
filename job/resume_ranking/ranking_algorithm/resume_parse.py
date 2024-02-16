8788b8caa4c56b00b7406890788924b1d3d8e53f
Suraj Pathak <surajpathak2057@gmail.com>
2024-02-17 00:28:45 +0545

    Minor improvement in resume parser and scoring algo
    -  Fix resume parser section detection
    - Enhanced experience extraction by replacing both En-dash '–' and Em-dash '—' with Hyphen '-' before NER
    - Handle cases like react.js and react in skill matching

--- job/resume_ranking/ranking_algorithm/resume_parse.py
+++ job/resume_ranking/ranking_algorithm/resume_parse.py
@@ -34,7 +34,9 @@ def extract_sections_from_pdf(pdf_path, keywords):
                         ):
                             # The text contains a keyword, so start a new section
                             current_section = s["text"].strip().upper()
-                            sections[current_section] = ""
+                            # Initialize the section only if it's seen for the first time
+                            if current_section not in sections:
+                                sections[current_section] = ""  
                         elif current_section is not None:
                             # This is not a section title, so append it to the current section
                             sections[current_section] += s["text"] + " "
